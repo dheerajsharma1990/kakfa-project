a346c53c6990bc07289f71cd9fae18011220c066
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2017-03-23 15:58:46 +0100

    compression decompression time taken.

--- gsm-choice-grabber/src/main/java/com/kafka/project/grabber/StringReaderWriter.java
+++ gsm-choice-grabber/src/main/java/com/kafka/project/grabber/StringReaderWriter.java
@@ -3,12 +3,15 @@ package com.kafka.project.grabber;
 import com.kafka.project.domain.AllMobiles;
 import com.kafka.project.domain.MobileRawData;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class StringReaderWriter {
 
@@ -22,12 +25,45 @@ public class StringReaderWriter {
             }
             builder.append("\n");
         }
-        Files.write(Paths.get("stringData.txt"), builder.toString().getBytes());
+        byte[] bytes = builder.toString().getBytes();
+        long startTime = System.currentTimeMillis();
+        byte[] compress = compress(bytes);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken to compress bytes " + (endTime - startTime) + " millis.");
+        Files.write(Paths.get("stringData.txt"), compress);
+        startTime = System.currentTimeMillis();
+        System.out.println("Time taken to write to file " + (startTime - endTime) + " millis.");
+    }
 
+    private byte[] compress(byte[] data) throws IOException {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        deflater.finish();
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer); // returns the generated code... index
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+        System.out.println("Original: " + data.length);
+        System.out.println("Compressed: " + output.length);
+        return output;
     }
 
     public AllMobiles getAllMobiles() throws Exception {
-        List<String> list = Files.readAllLines(Paths.get("stringData.txt"));
+        long startTime = System.currentTimeMillis();
+        byte[] bytes = Files.readAllBytes(Paths.get("stringData.txt"));
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken to read file while deserialization " + (endTime - startTime) + " millis.");
+        byte[] decompress = decompress(bytes);
+        startTime = System.currentTimeMillis();
+        System.out.println("Time taken to decompress bytes " + (startTime - endTime) + " millis.");
+        String string = new String(decompress);
+        List<String> list = Arrays.stream(string.split("\n")).collect(Collectors.toList());
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken to deserialize " + (endTime - startTime) + " millis. ");
         List<MobileRawData> mobileRawDatas = new ArrayList<>();
         int i = 0;
         boolean start = true;
@@ -50,17 +86,29 @@ public class StringReaderWriter {
         return new AllMobiles(mobileRawDatas);
     }
 
+    private static byte[] decompress(byte[] data) throws IOException, DataFormatException {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!inflater.finished()) {
+            int count = inflater.inflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+        System.out.println("Original: " + data.length);
+        System.out.println("De Compressed: " + output.length);
+        return output;
+    }
+
     public static void main(String[] args) throws Exception {
         StringReaderWriter stringWriter = new StringReaderWriter();
         ReadData readData = new ReadData();
         AllMobiles data = readData.getData();
-        long startTime = System.currentTimeMillis();
         stringWriter.writeInString(data);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Time Taken to write file " + (endTime - startTime) + " millis.");
         AllMobiles allMobiles = stringWriter.getAllMobiles();
-        startTime = System.currentTimeMillis();
-        System.out.println("Time taken to read from file " + (startTime - endTime ) + " millis.");
+        System.out.println();
     }
 
 }
