b8dff16754d9d15f1fb34c28c0ba64538f3a10f3
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2017-03-27 17:55:20 +0200

    compression decompression take 1.5 sec each..for 33 mb data..compression..and decompression

--- gsm-choice-grabber/src/main/java/com/kafka/project/grabber/ReadData.java
+++ gsm-choice-grabber/src/main/java/com/kafka/project/grabber/ReadData.java
@@ -1,19 +1,28 @@
 package com.kafka.project.grabber;
 
 import com.kafka.project.gsm.domain.RawMobileData;
+import io.protostuff.LinkedBuffer;
 import io.protostuff.ProtostuffIOUtil;
 import io.protostuff.Schema;
 import io.protostuff.runtime.RuntimeSchema;
+import org.apache.commons.io.IOUtils;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class ReadData {
+
+    private static final Schema<RawMobileData> rawMobileDataSchema = RuntimeSchema.getSchema(RawMobileData.class);
+    private static final int bufferSize = 2048;
 
     class AllMobiles implements Serializable {
 
@@ -40,47 +49,54 @@ public class ReadData {
     public static void main(String[] args) throws Exception {
         ReadData readData = new ReadData();
         List<RawMobileData> data = readData.getData();
-        RawMobileData rawMobileData = data.get(0);
-
-        System.out.println();
-    }
-
-    private static byte[] decompressGzip(byte[] data) {
-        try {
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
-            GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream);
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = gzipInputStream.read(buffer)) != -1) {
-                byteArrayOutputStream.write(buffer, 0, len);
-            }
-            byte[] bytes = byteArrayOutputStream.toByteArray();
-            //close resources
-            byteArrayOutputStream.close();
-            gzipInputStream.close();
-            return bytes;
-        } catch (IOException e) {
-            e.printStackTrace();
+        List<byte[]> protoBytes = new ArrayList<>();
+        for (RawMobileData rawMobileData : data) {
+            protoBytes.add(ProtostuffIOUtil.toByteArray(rawMobileData, rawMobileDataSchema, getApplicationBuffer()));
+            getApplicationBuffer().clear();
         }
-        return null;
+
+        long startTime = System.currentTimeMillis();
+        List<byte[]> bytes = protoBytes.stream().map(b -> compress(b)).collect(Collectors.toList());
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time to compress " + (endTime - startTime) + " millis.");
+
+        startTime = System.currentTimeMillis();
+        List<byte[]> uncompressed = bytes.stream().map(b -> decompress(b)).collect(Collectors.toList());
+        endTime = System.currentTimeMillis();
+        System.out.println("Time to decompress " + (endTime - startTime) + " millis.");
+
     }
 
-    private static byte[] compressGzip(byte[] data) {
+    private static byte[] decompress(byte[] data) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            IOUtils.copy(new GZIPInputStream(new ByteArrayInputStream(data)), out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return out.toByteArray();
+    }
+
+    private static byte[] compress(byte[] content) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try {
             GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
-            gzipOutputStream.write(data);
-            byte[] bytes = byteArrayOutputStream.toByteArray();
-            //close resources
+            gzipOutputStream.write(content);
             gzipOutputStream.close();
-            byteArrayOutputStream.close();
-            return bytes;
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        return null;
-
+        return byteArrayOutputStream.toByteArray();
     }
+
+    private static LinkedBuffer getApplicationBuffer() {
+        return localBuffer.get();
+    }
+
+    private static final ThreadLocal<LinkedBuffer> localBuffer = new ThreadLocal<LinkedBuffer>() {
+        public LinkedBuffer initialValue() {
+            return LinkedBuffer.allocate(bufferSize);
+        }
+    };
 
 }
